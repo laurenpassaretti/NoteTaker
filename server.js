b8c5ee2d76436e2410e708b283b03600896e7ca1
Lauren Doss <doss.laurene@gmail.com>
2020-04-04 15:29:18 -0700

    added './' in front of css and js files in notes.html to ensure the files were correctly linked

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 //Dependencies
 const http = require("http"); 
 const fs = require("fs"); 
-const express = require("express")
+const express = require("express"); 
+const path = require("path"); 
 
 var app = express()
 const PORT = process.env.PORT || 3000; 
+// var server = http.createServer(handleRequest); 
+
+//assists with data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 //html route to return the notes file
 app.get("/notes", function(req,res) {
-    res.sendFile(path.join(__dirname, "notes.html")); 
+    res.sendFile(path.join(__dirname, "./public/notes.html")); 
 }); 
 
 //html route to return to index.html
 app.get("*", function(req,res) {
-    res.sendFile(path.join(__dirname, "index.html")); 
+    res.sendFile(path.join(__dirname, "./public/index.html")); 
 })
 
 //code to "start" server
 app.listen(PORT, function() {
-    console.log(`Server is listen listening on ${PORT}`);
-}); 
+    console.log("App listening on PORT: " + PORT);
+  });
