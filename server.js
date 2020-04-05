6567e0d5a44618d47adce007f2b6f6b462821978
Lauren Doss <doss.laurene@gmail.com>
2020-04-05 10:53:00 -0700

    improved functionality of the GET and POST for api/notes so that it doesn't just default to home page

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require("http");
 const fs = require("fs"); 
 const express = require("express"); 
 const path = require("path"); 
-
-var app = express()
+var savedNotes = []
+var app = express();
 const PORT = process.env.PORT || 3000; 
 // var server = http.createServer(handleRequest); 
 
@@ -17,11 +17,39 @@ app.use(express.json());
 app.get("/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "./public/notes.html")); 
 }); 
+app.get("/api/notes", (req,res)=>{
+    fs.readFileSync("db/db.json", "utf-8", (err)=>{
+        if (err) throw err 
+    })
+    res.json(newNote); 
+})
 
 //html route to return to index.html
 app.get("*", function(req,res) {
     res.sendFile(path.join(__dirname, "./public/index.html")); 
 })
+// app.get("/api/notes", function(req,res) {
+//     res.sendFile(db.json)
+//   }); 
+// app.post("/notes", function(req,res) {
+//     var newNote = req.body; 
+//     fs.appendFileSync('./db/db.json', render([newNote]), (req,res) => {
+//     console.log("successfully created new note")
+//     console.log(newNote); 
+//     savedNotes.push(newNote); 
+    
+//     })
+    // console.log(newNote); 
+    // savedNotes.push(newNote); 
+    // res.json(newNote); 
+    // res.json(db.json); 
+
+    
+//   });
+//   app.get("/api/notes", function(req,res){
+//       res.json(db.json)
+//   })
+//   app.use('/api/notes', express.static(__dirname + 'db/db.json')); 
 
 //code to "start" server
 app.listen(PORT, function() {
