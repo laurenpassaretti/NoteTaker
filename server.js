01c854e53bc6ac0be234a85c549384c64a4272e2
Lauren Doss <doss.laurene@gmail.com>
2020-04-05 11:03:07 -0700

    Got api/notes to display information on db.json file instead of having issues with undefined variables

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Dependencies
-const http = require("http"); 
-const fs = require("fs"); 
-const express = require("express"); 
-const path = require("path"); 
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 var savedNotes = []
 var app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 // var server = http.createServer(handleRequest); 
 
 //assists with data parsing
@@ -14,44 +14,51 @@ app.use(express.json());
 
 
 //html route to return the notes file
-app.get("/notes", function(req,res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html")); 
-}); 
-app.get("/api/notes", (req,res)=>{
-    fs.readFileSync("db/db.json", "utf-8", (err)=>{
-        if (err) throw err 
-    })
-    res.json(newNote); 
-})
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.post("/notes", function (req, res) {
+    var newNote = req.body;
+    fs.appendFileSync('./db/db.json', { newNote }, (req, res) => {
+        console.log("successfully created new note")
+        console.log(newNote);
+        savedNotes.push(newNote);
 
-//html route to return to index.html
-app.get("*", function(req,res) {
-    res.sendFile(path.join(__dirname, "./public/index.html")); 
-})
-// app.get("/api/notes", function(req,res) {
-//     res.sendFile(db.json)
-//   }); 
-// app.post("/notes", function(req,res) {
-//     var newNote = req.body; 
-//     fs.appendFileSync('./db/db.json', render([newNote]), (req,res) => {
-//     console.log("successfully created new note")
-//     console.log(newNote); 
-//     savedNotes.push(newNote); 
-    
-//     })
+    })
+}); 
+    app.get("/api/notes", (req, res) => {
+        jsonFile = fs.readFileSync("db/db.json", "utf-8", (err) => {
+            if (err) throw err
+        })
+        res.json(jsonFile);
+    })
+
+    //html route to return to index.html
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./public/index.html"));
+    })
+
+    // app.post("/notes", function(req,res) {
+    //     var newNote = req.body; 
+    //     fs.appendFileSync('./db/db.json', render([newNote]), (req,res) => {
+    //     console.log("successfully created new note")
+    //     console.log(newNote); 
+    //     savedNotes.push(newNote); 
+
+    //     })
     // console.log(newNote); 
     // savedNotes.push(newNote); 
     // res.json(newNote); 
     // res.json(db.json); 
 
-    
-//   });
-//   app.get("/api/notes", function(req,res){
-//       res.json(db.json)
-//   })
-//   app.use('/api/notes', express.static(__dirname + 'db/db.json')); 
 
-//code to "start" server
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+    //   });
+    //   app.get("/api/notes", function(req,res){
+    //       res.json(db.json)
+    //   })
+    //   app.use('/api/notes', express.static(__dirname + 'db/db.json')); 
+
+    //code to "start" server
+    app.listen(PORT, function () {
+        console.log("App listening on PORT: " + PORT);
+    });
