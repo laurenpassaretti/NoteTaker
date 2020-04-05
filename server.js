f510d7f45160c475705f87b2ef6aa59e7cfb3afc
Lauren Doss <doss.laurene@gmail.com>
2020-04-05 13:14:27 -0700

    added to POST method

--- server.js
+++ server.js
@@ -17,48 +17,51 @@ app.use(express.json());
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+//html route to return to index.html
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+//route to display test in the db.json file
+app.get("/api/notes", (req, res) => {
+    jsonFile = fs.readFileSync("db/db.json", "utf-8", (err) => {
+        if (err) throw err
+    })
+    res.json(jsonFile);
+})
 app.post("/notes", function (req, res) {
     var newNote = req.body;
-    fs.appendFileSync('./db/db.json', { newNote }, (req, res) => {
+    fs.appendFileSync('db/db.json', {newNote}, (req, res) => {
         console.log("successfully created new note")
         console.log(newNote);
-        savedNotes.push(newNote);
+        jsonFile.push(newNote);
 
     })
-}); 
-    app.get("/api/notes", (req, res) => {
-        jsonFile = fs.readFileSync("db/db.json", "utf-8", (err) => {
-            if (err) throw err
-        })
-        res.json(jsonFile);
-    })
-
-    //html route to return to index.html
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
-    })
-
-    // app.post("/notes", function(req,res) {
-    //     var newNote = req.body; 
-    //     fs.appendFileSync('./db/db.json', render([newNote]), (req,res) => {
-    //     console.log("successfully created new note")
-    //     console.log(newNote); 
-    //     savedNotes.push(newNote); 
-
-    //     })
-    // console.log(newNote); 
-    // savedNotes.push(newNote); 
-    // res.json(newNote); 
-    // res.json(db.json); 
+});
 
 
-    //   });
-    //   app.get("/api/notes", function(req,res){
-    //       res.json(db.json)
-    //   })
-    //   app.use('/api/notes', express.static(__dirname + 'db/db.json')); 
 
-    //code to "start" server
-    app.listen(PORT, function () {
-        console.log("App listening on PORT: " + PORT);
-    });
+
+// app.post("/notes", function(req,res) {
+//     var newNote = req.body; 
+//     fs.appendFileSync('./db/db.json', render([newNote]), (req,res) => {
+//     console.log("successfully created new note")
+//     console.log(newNote); 
+//     savedNotes.push(newNote); 
+
+//     })
+// console.log(newNote); 
+// savedNotes.push(newNote); 
+// res.json(newNote); 
+// res.json(db.json); 
+
+
+//   });
+//   app.get("/api/notes", function(req,res){
+//       res.json(db.json)
+//   })
+//   app.use('/api/notes', express.static(__dirname + 'db/db.json')); 
+
+//code to "start" server
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
