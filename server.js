f8b1dde5f10af60cf459a223df1bfb2e0c1491b0
Lauren Doss <doss.laurene@gmail.com>
2020-04-05 13:48:02 -0700

    functionality is working so that now the new notes are appearing in the json file and are displaying once the get api/notes is called

--- server.js
+++ server.js
@@ -11,33 +11,51 @@ const PORT = process.env.PORT || 3000;
 //assists with data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+// app.use('/assets', express.static('/public'));
 
 
 //html route to return the notes file
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-//html route to return to index.html
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-})
 //route to display test in the db.json file
 app.get("/api/notes", (req, res) => {
     jsonFile = fs.readFileSync("db/db.json", "utf-8", (err) => {
         if (err) throw err
     })
     res.json(jsonFile);
+}); 
+//html route to return to index.html
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
-app.post("/notes", function (req, res) {
+
+
+app.post("/api/notes", function (req, res) {
+    jsonFile= fs.readFileSync("db/db.json", "utf-8")
+    jsonFile = JSON.parse(jsonFile); 
+    req.body.id = jsonFile.length; 
     var newNote = req.body;
-    fs.appendFileSync('db/db.json', {newNote}, (req, res) => {
+    jsonFile.push(newNote)
+    jsonFile = JSON.stringify(jsonFile); 
+    fs.writeFileSync('db/db.json', jsonFile, (req, res) => {
         console.log("successfully created new note")
-        console.log(newNote);
-        jsonFile.push(newNote);
+        if (err) throw err; 
+        res.json(jsonFile)
 
     })
 });
-
+app.delete("/api/notes/:id", function(req,res){
+    let noteID = req.params.id;
+   
+    for(var i = 0; i < notes.length; i++){
+        if(noteID === notes[i].id){
+            delete notes[i];
+        }
+    }
+}); 
 
 
 
